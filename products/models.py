ec36049577d679f528212410ec4ec1b7dfb60dca
Nurpolat <80279115+Khayratdinov@users.noreply.github.com>
2022-11-10 21:31:24 +0000

    chore: say 👋 Hello from CodeSpaces 😁

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
